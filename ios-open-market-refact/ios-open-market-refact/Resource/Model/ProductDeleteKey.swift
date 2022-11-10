d8ec3671639e9b2e46519982a219a90fd5e7983c
Kiwi <toskw@naver.com>
2022-11-10 21:30:59 +0900

    feat: Post, Patch, Delete 기능을 위한 Network 모델 구현
    - MultipartFormData 생성 메서드 구현
    - Data 타입 확장

--- ios-open-market-refact/ios-open-market-refact/Resource/Model/ProductDeleteKey.swift
+++ ios-open-market-refact/ios-open-market-refact/Resource/Model/ProductDeleteKey.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-struct ProductDeleteKey: Codable {
+struct ProductDeleteKey: Encodable {
     let secret: String
 }
--- ios-open-market-refact/ios-open-market-refact/Resource/Utill/Network/APIRequestProtocol.swift
+++ ios-open-market-refact/ios-open-market-refact/Resource/Utill/Network/APIRequestProtocol.swift
@@ -32,12 +32,29 @@ enum URLAdditionalPath {
     }
 }
 
+enum HTTPBody {
+    case multiPartForm(_ form: MultiPartForm)
+    case json(_ json: Data)
+}
+
+struct MultiPartForm {
+    let boundary: String
+    let jsonData: Data
+    let images: [ProductImage]
+}
+
+struct ProductImage {
+    let name: String
+    let data: Data
+    let type: String
+}
+
 protocol APIRequest {
     var method: HTTPMethod { get }
     var baseURL: String { get }
     var headers: [String: String]? { get }
     var query: [String: String]? { get }
-    var body: Data? { get }
+    var body: HTTPBody? { get }
     var path: String { get }
 }
 
@@ -55,12 +72,69 @@ extension APIRequest {
         
         var urlRequest = URLRequest(url: url)
         
-        urlRequest.httpBody = body
+        urlRequest.httpBody = createHTTPBody()
         urlRequest.httpMethod = method.type
         headers?.forEach {
             urlRequest.addValue($0.value, forHTTPHeaderField: $0.key)
         }
         
         return urlRequest
+    }
+}
+
+extension APIRequest {
+    private func createHTTPBody() -> Data {
+        guard let body = self.body else { return Data() }
+        
+        switch body {
+        case .json(let json):
+            return json
+        case .multiPartForm(let form):
+            return createMultiPartFormBody(form: form)
+        }
+    }
+    
+    private func createMultiPartFormBody(form: MultiPartForm) -> Data {
+        let lineBreak = "\r\n"
+        var requestBody = Data()
+        
+        requestBody.append(createMultipartFormJsonData(boundary: form.boundary,
+                                                       json: form.jsonData))
+        
+        form.images.forEach
+        {
+            requestBody.append(createMultipartFormImageData(boundary: form.boundary,
+                                                            image: $0))
+        }
+        
+        requestBody.append("\(lineBreak)--\(form.boundary)--\(lineBreak)")
+        print(String(decoding: requestBody, as: UTF8.self))
+        return requestBody
+    }
+    
+    private func createMultipartFormJsonData(boundary: String, json: Data) -> Data {
+        let lineBreak = "\r\n"
+        var paramsBody = Data()
+        
+        paramsBody.append("\(lineBreak)--\(boundary + lineBreak)")
+        paramsBody.append("Content-Disposition: form-data; name=\"params\"\(lineBreak)")
+        paramsBody.append("Content-Type: application/json \(lineBreak + lineBreak)")
+        paramsBody.append(json)
+        
+        return paramsBody
+    }
+    
+    private func createMultipartFormImageData(boundary: String, image: ProductImage) -> Data {
+        let lineBreak = "\r\n"
+        let fileName = image.name + "." + image.type
+        let fileType = "image/\(image.type)"
+        var imageBody = Data()
+        
+        imageBody.append("\(lineBreak)--\(boundary + lineBreak)")
+        imageBody.append("Content-Disposition: form-data; name=\"images\"; filename=\"\(fileName)\"\(lineBreak)")
+        imageBody.append("Content-Type: \(fileType) \(lineBreak + lineBreak)")
+        imageBody.append(image.data)
+        
+        return imageBody
     }
 }

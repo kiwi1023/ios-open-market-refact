92fdf18de9921f9667695da1393467ea5929ddcd
yusw10 <yusw10@naver.com>
2022-11-10 16:13:10 +0900

    feat: 상품 리스트 화면에 사용될 모델 정의
    - 전체 상품 리스트르를 받기 위한 ProductList모델 타입 구현
    - 각 상품 기본 정보를 담을 Product 모델 구현
    - 화폐 정보를 표현할 Currency 열거형 구현
    - 파일 세분화 기초작업

---
+++ ios-open-market-refact/ios-open-market-refact/Resource/Model/CurrencyEnum.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyEnum.swift
+//  ios-open-market-refact
+//
+//  Created by 유한석 on 2022/11/10.
+//
+
+import Foundation
+
+enum Currency: String, Decodable {
+    case krw = "KRW"
+    case usd = "USD"
+}

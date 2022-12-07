5d11b7a8ec526887407077d08d3971ee59ab6d6f
apwierk2451 <apwierk2451@gmail.com>
2022-12-07 14:46:30 +0900

    refact: 세로모드 고정 코드 추가

--- ios-open-market-refact/ios-open-market-refact/Resource/AppDelegate.swift
+++ ios-open-market-refact/ios-open-market-refact/Resource/AppDelegate.swift
@@ -19,6 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 }
 

32f7cda583601875323799a827a8133037a7b9dc
yusw10 <yusw10@naver.com>
2022-12-18 00:09:14 +0900

    test: BannerViewModel 테스트 코드 작성
    - 배너 이미지의 url 목록을 가져오는 지를 검증하는 코드 작성
    - 배너 이미지 로드 중 에러가 발생할 경우의 에러 핸들링을 검증하는 코드 작성

---
+++ ios-open-market-refact/ios-open-market-refact-Tests/BannerViewModelTest/BannerViewModelTest.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewModelTest.swift
+//  ios-open-market-refact-Tests
+//
+//  Created by 이은찬 on 2022/12/17.
+//
+
+import XCTest
+@testable import ios_open_market_refact
+
+final class BannerViewModelTest: XCTestCase {
+    
+    struct MockSession: SessionProtocol {
+        var result: Result<Data, Error>
+        func dataTask(with request: APIRequest, completionHandler: @escaping (Result<Data, Error>) -> Void) {
+            completionHandler(result)
+        }
+    }
+    
+    func test_유효한_데이터_값이_주어졌을_경우확인() {
+        // given
+        let viewModel = BannerViewModel(
+            networkAPI: MockSession(result: .success(MockData(fileName: "BannerImageUrlMockData").data!))
+        )
+        let bannerImageUrlLoadAction = Observable<Void>(())
+        let input = BannerViewModel.Input(loadBannerImagesAction: bannerImageUrlLoadAction)
+        let output = viewModel.transform(input: input)
+
+        // when
+        let imageUrlCounts = output.loadBannerImagesOutPut.value.count
+        
+        // then
+        XCTAssertEqual(imageUrlCounts, 6)
+    }
+    
+    func test_유효하지_않는_데이터_값이_주어졌을_경우확인() {
+        // given, when
+        var result = ""
+        let viewModel = BannerViewModel(networkAPI: MockSession(result: .failure(APIError.invalidData)))
+        
+        viewModel.onErrorHandling = { _ in
+            result = "error"
+        }
+        
+        let bannerImageUrlLoadAction = Observable<Void>(())
+        let input = BannerViewModel.Input(loadBannerImagesAction: bannerImageUrlLoadAction)
+        _ = viewModel.transform(input: input)
+        
+        // then
+        XCTAssertEqual(result, "error")
+    }
+}
